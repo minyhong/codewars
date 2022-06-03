fb6a7827726ee3f43254458a84b79691f81b53b7
Min Hong <miny.hong93@gmail.com>
2022-06-03 16:30:25 -0400

    form the minimum

---
+++ js/solution033.js
@@ -0,0 +1,20 @@
+// Task
+
+// Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates).
+
+// Notes:
+
+// Only positive integers will be passed to the function (> 0 ), no negatives or zeros.
+// Input >> Output Examples
+
+// minValue ({1, 3, 1})  ==> return (13)
+// Explanation:
+
+// (13) is the minimum number could be formed from {1, 3, 1} , Without duplications
+
+function minValue(values){
+    // create new array of values without any repeats
+    let newValues = [...new Set(values)]
+    // return the new values sorted in numerical order, join, and change data type to number
+    return Number(newValues.sort((a, b) => a - b).join(''))
+  }
