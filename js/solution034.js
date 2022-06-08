4f3cf33acd5842b2c48f893c75078cb732b475f4
Min Hong <miny.hong93@gmail.com>
2022-06-08 17:02:05 -0400

    reverse every other word in the string

---
+++ js/solution034.js
@@ -0,0 +1,11 @@
+// Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
+
+// Not gonna lie, this is hideous code lolz
+
+function reverse(str){
+    return str.split(' ').map((elem, index) => index % 2 === 1 ? reverseString(elem) : elem).join(' ').split('   ').join('')
+  }
+                       
+function reverseString(str) {
+    return str.split('').reverse().join('')
+}
